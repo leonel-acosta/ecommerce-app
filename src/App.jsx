e6a629112524c4135c9db3074ed37d4ae36967a7
Leonel Acosta <vsiostudioberlin@gmail.com>
2023-06-07 16:42:43 +0200

    creando repositorio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+function App() {
+
+  return ( <div>
+    <Navbar />
+
+    <h1> Hola </h1>
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () =>{
+    
+    return <div>
+        <h2>test</h2>
+        <h3>Aca algo mas</h3>
+    </div>
+}
+
+export default Navbar;
